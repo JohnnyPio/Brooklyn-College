8439a335c20dd0f89d2ba789d1b87b32d217aa48
JohnnyPio <john.s.piotrowski@gmail.com>
2025-02-02 16:45:51 -0500

    2.

--- CISC 7510X/HW1/CISC 7510X HW#1.sql
+++ CISC 7510X/HW1/CISC 7510X HW#1.sql
@@ -1,6 +1,21 @@
+-- -- 1. 
+-- select
+-- 	description
+-- from
+-- 	store.product
+-- where
+-- 	product.productid=42
+
+-- 2.
 select
-	description
+	fname,
+	lname,
+	street1,
+	street2,
+	city,
+	state,
+	zip
 from
-	store.product
+	store.customer
 where
-	product.productid=42
+	customer.customerid=42
