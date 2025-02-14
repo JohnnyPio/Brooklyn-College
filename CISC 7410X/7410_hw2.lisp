311b3752861a9683d89fb185534e02571d69429f
JohnnyPio <john.s.piotrowski@gmail.com>
2025-02-14 17:03:06 -0500

    4.1 and 4.2

--- CISC 7410X/7410_hw2.lisp
+++ CISC 7410X/7410_hw2.lisp
@@ -1,3 +1,13 @@
-;(defun square)
+;John Piotrowski - 7410x - HW2
 
+;;; 4.1
+(defun make-even (x)
+  (if (oddp x) (+ x 1) x))
+
+;;; 4.2
+(defun further (x)
+  (if (> x 0) (+ x 1) (- x 1)))
+; x=0, that makes the condition NIL so it subtracts 1.
+
+;;; 4.3
 
