51f30db5b8ad0659105019730ecedbf1bdbe5502
JohnnyPio <john.s.piotrowski@gmail.com>
2025-02-15 10:53:57 -0500

    4.3-4.5

--- CISC 7410X/7410_hw2.lisp
+++ CISC 7410X/7410_hw2.lisp
@@ -10,4 +10,27 @@
 ; x=0, that makes the condition NIL so it subtracts 1.
 
 ;;; 4.3
+(defun my-not (x)
+  (if x nil t))
 
+
+;;;4.4
+(defun ordered (x y)
+  (if (< x y) (list x y) (list y x)))
+
+;;;4.5
+(defun compare (x y)
+  (cond ((equal x y) 'numbers-are-the-same)
+  ((< x y) 'first-is-smaller)
+  ((> x y) 'first-is-bigger)))
+
+;first-is-bigger (compare 9 1)
+;first-is-smaller (compare (+ 2 2) 5)
+;numbers-are-the-same (compare 6 (* 2 3))
+
+;;;4.5
+(defun my-abs (x) 
+  (cond ((< x 0) (* x -1))
+        (t x)))
+
+;;;4.6
