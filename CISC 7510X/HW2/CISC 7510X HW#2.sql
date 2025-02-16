d3c4f9f97699f7ab56b48b4be0f897f5dcbe6270
JohnnyPio <john.s.piotrowski@gmail.com>
2025-02-16 17:27:37 -0500

    working through 5.

--- CISC 7510X/HW2/CISC 7510X HW#2.sql
+++ CISC 7510X/HW2/CISC 7510X HW#2.sql
@@ -6,7 +6,7 @@ SET search_path = main, "$user", public;
 -- select count(username)
 -- from doorlog
 -- where event = 'E'
--- 	and doorid = 1
+-- 	and doorid = 1;
 
 -- 2.
 -- with users_entering_2 as (
@@ -22,7 +22,7 @@ SET search_path = main, "$user", public;
 -- 		and doorid = 2
 -- )
 -- select users_entering_2.count_users - users_exiting_2.count_users
--- from users_entering_2, users_exiting_2
+-- from users_entering_2, users_exiting_2;
 
 -- 3.
 -- with users_entering_frontandback as (
@@ -38,22 +38,57 @@ SET search_path = main, "$user", public;
 -- 		and doorid in (2, 3)
 -- )
 -- select users_entering_frontandback.count_users - users_exiting_frontandback.count_users
--- from users_entering_frontandback, users_exiting_frontandback
+-- from users_entering_frontandback, users_exiting_frontandback;
 
 -- 4.
-with users_entering_frontandback as (
-	select count(username) as count_users
+-- with users_entering_frontandback as (
+-- 	select count(username) as count_users
+-- 	from doorlog
+-- 	where event = 'E'
+-- 		and doorid in (2, 3)
+-- 		and tim <= '2024-07-04 22:00:00'
+-- ),
+-- users_exiting_frontandback as(
+-- 	select count(username) as count_users
+-- 	from doorlog
+-- 	where event = 'X'
+-- 		and doorid in (2, 3)
+-- 		and tim <= '2024-07-04 22:00:00'
+-- )
+-- select users_entering_frontandback.count_users - users_exiting_frontandback.count_users
+-- from users_entering_frontandback, users_exiting_frontandback;
+
+-- 5.
+with all_days_2021 as (
+	select generate_series('2021-01-01'::date, '2021-12-31'::date, '1 day') as everyday
+),
+users_entering_floor42 as (
+	select 
+		count(username) as count_users,
+		date(tim) as event_day
 	from doorlog
 	where event = 'E'
-		and doorid in (2, 3)
-		and tim <= '2024-07-04 22:00:00'
+		and doorid  = 7
+		and tim >= '2021-01-01 00:00:00'
+		and tim < '2022-01-01 00:00:00'
+	group by DATE(tim)
 ),
-users_exiting_frontandback as(
-	select count(username) as count_users
+users_exiting_floor42 as(
+	select 
+		count(username) as count_users,
+		date(tim) as event_day
 	from doorlog
 	where event = 'X'
-		and doorid in (2, 3)
-		and tim <= '2024-07-04 22:00:00'
+		and doorid  = 7
+		and tim >= '2021-01-01 00:00:00'
+		and tim < '2022-01-01 00:00:00'
+	group by DATE(tim)
 )
-select users_entering_frontandback.count_users - users_exiting_frontandback.count_users
-from users_entering_frontandback, users_exiting_frontandback
+select 
+	all_days_2021.everyday,
+	users_entering_floor42.count_users,
+	users_exiting_floor42.count_users,
+	coalesce(users_entering_floor42.count_users, 0) - coalesce(users_exiting_floor42.count_users, 0),
+	from all_days_2021
+	left join users_entering_floor42 on all_days_2021.everyday = users_entering_floor42.event_day
+	left join users_exiting_floor42 on all_days_2021.everyday = users_exiting_floor42.event_day
