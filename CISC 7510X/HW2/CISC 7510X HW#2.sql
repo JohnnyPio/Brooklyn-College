efce9f5321c957d411c13f61f9825297595e3887
JohnnyPio <john.s.piotrowski@gmail.com>
2025-02-16 16:16:12 -0500

    up to 3.

--- CISC 7510X/HW2/CISC 7510X HW#2.sql
+++ CISC 7510X/HW2/CISC 7510X HW#2.sql
@@ -3,12 +3,23 @@ SET search_path = main, "$user", public;
 
 -- 1.
 -- I was a bit unclear about the wording of this question - I interpreted it as total users not distinct users.
-select count(username)
-from doorlog
-where event = 'E'
-	and doorid = 1
-
--- 2.
 -- select count(username)
 -- from doorlog
 -- where event = 'E'
+-- 	and doorid = 1
+
+-- 2.
+with users_entering_2 as (
+	select count(username) as count_users
+	from doorlog
+	where event = 'E'
+		and doorid = 2
+),
+users_exiting_2 as(
+	select count(username) as count_users
+	from doorlog
+	where event = 'X'
+		and doorid = 2
+)
+select users_entering_2.count_users - users_exiting_2.count_users
+from users_entering_2, users_exiting_2
