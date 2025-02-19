678cc27ce3bfbae3491db846800a6db86672395c
JohnnyPio <john.s.piotrowski@gmail.com>
2025-02-19 15:03:49 -0500

    6.1

--- CISC 7410X/HW3/JohnPiotrowski_7410x_HW3.lisp
+++ CISC 7410X/HW3/JohnPiotrowski_7410x_HW3.lisp
@@ -1,4 +1,11 @@
 ;John Piotrowski - 7410x - HW3
 ;6.1-6.42, 7.1-7.29
 
+;;;6.1. (NTH 4 '(A B C)) is equal to NIL because the 4th CDR of is NIL and the CAR of NIL is NIL.
+
+;;;6.2 The value of (NTH 3 '(A B C . D)) is 
+
+
+
+
 
