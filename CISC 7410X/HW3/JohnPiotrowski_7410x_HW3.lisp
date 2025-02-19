f0b2d8fbfaedac1706ba2a5c0084d2d75a69b6b2
JohnnyPio <john.s.piotrowski@gmail.com>
2025-02-19 15:29:28 -0500

    up to 6.5

--- CISC 7410X/HW3/JohnPiotrowski_7410x_HW3.lisp
+++ CISC 7410X/HW3/JohnPiotrowski_7410x_HW3.lisp
@@ -3,7 +3,13 @@
 
 ;;;6.1. (NTH 4 '(A B C)) is equal to NIL because the 4th CDR of is NIL and the CAR of NIL is NIL.
 
-;;;6.2 The value of (NTH 3 '(A B C . D)) is 
+;;;6.2. The value of (NTH 3 '(A B C . D)) is an error of TYPE-ERROR. The 3rd CDR is equal to D and the CAR of D is an error because D is not a list.
+
+;;;6.3. (LAST '(ROSEBUD)) returns (ROSEBUD)
+
+;;;6.4. (LAST '((A B C))) returns ((A B C)) because there is only 1 top-level list element and the last of that list returns that element.
+
+
 
 
 
