70a040c35f5718c26762e53991cca8bc729ce269
JohnnyPio <john.s.piotrowski@gmail.com>
2025-02-21 15:49:17 -0500

    to 41b

--- CISC 7410X/HW3/JohnPiotrowski_7410x_HW3.lisp
+++ CISC 7410X/HW3/JohnPiotrowski_7410x_HW3.lisp
@@ -260,3 +260,34 @@
 ;;  (D))
 
 ;;;6.41.
+(defvar rooms
+             '((living-room (north front-stairs)
+                            (south dining-room)
+                            (east kitchen))
+               (upstairs-bedroom (west library)
+                                 (south front-stairs))
+               (dining-room (north living-room)
+                            (east pantry)
+                            (west downstairs-bedroom))
+               (kitchen (west living-room)
+                        (south pantry))
+               (pantry (north kitchen)
+                       (west dining-room))
+               (downstairs-bedroom (north back-stairs)
+                                   (east dining-room))
+               (back-stairs (south downstairs-bedroom)
+                            (north library))
+               (front-stairs (north upstairs-bedroom)
+                             (south living-room))
+               (library (east upstairs-bedroom)
+                        (south back-stairs))))
+
+;;;6.41a
+(defun choices (room)
+  (rest (assoc room rooms))
+  )
+
+;;;6.41b
+(defun look (dir room)
+  ()
+  )
