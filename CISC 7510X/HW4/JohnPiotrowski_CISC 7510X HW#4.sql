f1fbd80179e956d3ea7fa38e6eecfb9c6f80a6c9
JohnnyPio <john.s.piotrowski@gmail.com>
2025-03-05 17:05:47 -0500

    7510 HW4 queries working, but need to cleanup

--- CISC 7510X/HW4/JohnPiotrowski_CISC 7510X HW#4.sql
+++ CISC 7510X/HW4/JohnPiotrowski_CISC 7510X HW#4.sql
@@ -2,8 +2,10 @@
 -- SET search_path = public, "$user", public;
 
 with msft_cts as (
-	select *,
-		tdate as cnt_tdate
+	select 
+	tdate as cnt_tdate,
+	symbol as cnt_symbol,
+	*
 	from cts
 	where cts.symbol = 'MSFT'
 ),
@@ -18,28 +20,31 @@ msft_splits as (
 	where splits.symbol = 'MSFT'
 ),
 all_3_combined as (
-select *
+select *,
+lag(close) over (partition by cnt_symbol order by cnt_tdate) as day_before_close_price
 from msft_cts
 left join msft_dividend on msft_cts.tdate = msft_dividend.tdate
 left join msft_splits on msft_cts.tdate = msft_splits.tdate
 where msft_cts.tdate = '1987-01-05'
+	or msft_cts.tdate = '2003-02-14'
 	or msft_cts.tdate = '2004-11-12'
 	or msft_cts.tdate = '2004-11-15'
+	or msft_cts.tdate = '1987-09-18'
+	or msft_cts.tdate = '1987-09-17'
 	or msft_splits.tdate IS NOT NULL
 	or msft_dividend.tdate IS NOT NULL
 order by msft_cts.tdate asc
 ),
 daily_prct_basic as (
-	select 	*,
-		-- there will need to be another column for a windowing function returning the last day's close value for div and split
+	select 
+		*,
 		case
-			WHEN dividend is null and post is null THEN (all_3_combined.close-all_3_combined.open)/all_3_combined.open
-			-- WHEN dividend is not null THEN 
-			-- WHEN post is not null THEN 
+			WHEN dividend is null and post is null THEN 100*(close-open)/open
+			WHEN dividend is not null THEN 100*(close/(day_before_close_price-dividend)-1)
+			WHEN post is not null THEN 100*((close/(day_before_close_price*(pre/(post*1.0))))-1)
 			else NULL
 		end as daily_prct
 	from all_3_combined
 )
 select *
-	
 from daily_prct_basic
