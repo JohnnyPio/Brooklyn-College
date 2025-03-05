c0103985817d87ab3ea8956e34c374ffce9d96f5
JohnnyPio <john.s.piotrowski@gmail.com>
2025-03-05 16:26:07 -0500

    DB work

--- CISC 7510X/Notes/20250227_Notes.sql
+++ CISC 7510X/Notes/20250227_Notes.sql
@@ -3,5 +3,5 @@
   --he didn't need to unzip anything
   --doing this with ubuntu looked more fun
   --he didn't need to do any access management due to STDIN
-copy split from STDIN with DELIMITER ',' NULL '' --Missed the NULL which adds handling
+copy cts from STDIN with DELIMITER ',' NULL '' --Missed the NULL which adds handling
 
