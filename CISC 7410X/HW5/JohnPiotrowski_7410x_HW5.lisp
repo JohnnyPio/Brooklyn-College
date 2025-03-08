e8dc758c685f4e92745ec2635a8aaa723d4f57db
JohnnyPio <john.s.piotrowski@gmail.com>
2025-03-08 14:16:59 -0500

    WIP Walking as a fall-back

--- CISC 7410X/HW5/JohnPiotrowski_7410x_HW5.lisp
+++ CISC 7410X/HW5/JohnPiotrowski_7410x_HW5.lisp
@@ -130,5 +130,18 @@
            :preconds '(locked-bike)
            :add-list '(at-home)
            :del-list '(locked-bike))
+  ;; ;;Walk
+  ;; It would be cool to have walking as a back-up plan in case everything else fails. However, I don't know how for the precond to ONLY contain (class over). Right now, it requires the other preconds.
+  ;; (make-op :action 'walk-home-in-an-hour
+  ;;          :preconds '(member class-over *state*)
+  ;;          :add-list '(walking)
+  ;;          :del-list '(in-class))
+  ;; (make-op :action 'this-is-longer-than-ideal
+  ;;          :preconds '(walking)
+  ;;          :add-list '(being-miserable))
+  ;; (make-op :action 'finally-home
+  ;;          :preconds '(walking being-miserable)
+  ;;          :add-list '(at-home)
+  ;;          :del-list '(walking being-miserable))
   ))
  
