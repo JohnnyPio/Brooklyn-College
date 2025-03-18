13fe2f889830fdb6e75c8c8f9f0eda887477e5ec
JohnnyPio <john.s.piotrowski@gmail.com>
2025-03-18 00:06:27 -0400

    FINALLY got the month filtering working - way too much time

--- CISC_7510X/HW5/JohnPiotrowski_CISC 7510X HW#5.sql
+++ CISC_7510X/HW5/JohnPiotrowski_CISC 7510X HW#5.sql
@@ -1,16 +1,18 @@
 -- John Piotrowski - HW5 - 7510x
-SET search_path = public, "$user", public;
+-- SET search_path = public, "$user", public;
 
 with num_trading_days_2013 as (
 	select count(distinct tdate) as yearly_trading_days
 	from daily_prcnt
 	where extract(year from tdate) = 2013
+	group by symbol
 ),
 num_trading_days_dec2013 as (
 	select count(distinct tdate) as dec_trading_days
 	from daily_prcnt
 	where extract(year from tdate) = 2013
 	and extract(month from tdate) = 12
+	group by symbol
 ),
 valid_2013_stocks as (
 	select *
@@ -18,19 +20,28 @@ valid_2013_stocks as (
 	where extract(year from tdate) = 2013
 		and symbol not like '%-%' 			-- Filter out pink sheets and OTC Bulletin Board and other dashed symbols
 		and prcnt is not null
-	group by tdate, symbol, prcnt
-	having extract(month from min(tdate)) = '01'		-- Filter out stocks that weren't trading in January 2013 like 'ANTH'
+),
+min_max_tdate_per_symbol as (
+    select symbol, 
+	extract(month from min(tdate)) as first_trade_month,
+	extract(month from max(tdate)) as last_trade_month
+    from valid_2013_stocks
+    group by symbol
 ),
 daily_trades_greater_than_10mil as (
 	select
 		valid_2013_stocks.tdate,
 		valid_2013_stocks.symbol,
-		valid_2013_stocks.prcnt
+		valid_2013_stocks.prcnt,
+		first_trade_month,
+		last_trade_month
 	from valid_2013_stocks
 	join cts on valid_2013_stocks.symbol = cts.symbol and valid_2013_stocks.tdate = cts.tdate
-	cross join num_trading_days_2013
-	group by valid_2013_stocks.tdate, valid_2013_stocks.symbol, valid_2013_stocks.prcnt, cts.close, cts.volume
+	join min_max_tdate_per_symbol on valid_2013_stocks.symbol = min_max_tdate_per_symbol.symbol
+	group by valid_2013_stocks.symbol, valid_2013_stocks.tdate, valid_2013_stocks.prcnt, cts.close, cts.volume, first_trade_month, last_trade_month
 	having cts.close * cts.volume > 10000000	-- est. daily trading total
+		and first_trade_month = 1				-- Filter out stocks that weren't trading in Jan 2013 like 'ATHM'
+		and last_trade_month = 12				-- Filter out stocks that weren't trading in Dec 2013
 ),
 min_prcnt_calc as (
 	select min(prcnt) as min_prcnt
@@ -52,8 +63,6 @@ stock_pairs_2013 as (
 		s2.shifted_log_value as s2_log
 	from log_calcs s1
 	left join log_calcs s2 on s2.tdate = s1.tdate and s2.symbol < s1.symbol
-	-- where s1.symbol like '%ATHM%' 
-	-- 	-- and s2.symbol like '%AAPL%'
 ),
 pearson_calcs_2013 as (
 	select
@@ -104,6 +113,7 @@ pairs_and_rs as (
 calculate_investments as (
 	select
 		stock_pair,
+		-- These both need be per symbol as some stocks were traded on different days
 		500000*(1+(ydp1.year_avg_prcnt/100)*yearly_trading_days) + 500000*(1+(ydp2.year_avg_prcnt/100)*yearly_trading_days) as invested_1mil_all2013,
 		500000*(1+(ddp1.dec_avg_prcnt/100)*dec_trading_days) + 500000*(1+(ddp2.dec_avg_prcnt/100)*dec_trading_days) as invested_1mil_dec2013
 	from pairs_and_rs
@@ -114,15 +124,24 @@ calculate_investments as (
 	cross join num_trading_days_2013
 	cross join num_trading_days_dec2013
 )
-select
-	prs.stock_pair,
-	stock_1,
-	stock_2,
-	r_2013,
-	r_december,
-	ci.invested_1mil_all2013,
-	ci.invested_1mil_dec2013
-	from pairs_and_rs prs
-	join calculate_investments ci on ci.stock_pair = prs.stock_pair
-	order by ci.invested_1mil_dec2013 desc
-	limit 10
+-- select
+-- 	prs.stock_pair,
+-- 	stock_1,
+-- 	stock_2,
+-- 	r_2013,
+-- 	r_december,
+-- 	ci.invested_1mil_all2013,
+-- 	ci.invested_1mil_dec2013
+-- 	from pairs_and_rs prs
+-- 	join calculate_investments ci on ci.stock_pair = prs.stock_pair
+-- 		where stock_1 like '%MSFT%'
+-- 		and stock_1 like '%GOOG%'
+-- 	order by ci.invested_1mil_dec2013 desc
+-- 	limit 10
+
+select *
+from daily_trades_greater_than_10mil
+where symbol like '%MSFT%'
+	or symbol like '%GOOG%'
+	or symbol like '%ATHM%'
+	
