5603cf0486a77e8b7adcdb17acf259dd6d5750c1
JohnnyPio <john.s.piotrowski@gmail.com>
2025-04-27 15:01:20 -0400

    HW6 finalized

--- CISC_7510X/HW6/JohnPiotrowski_CISC 7510X HW#6.sql
+++ CISC_7510X/HW6/JohnPiotrowski_CISC 7510X HW#6.sql
@@ -1,6 +1,7 @@
---Create Table statements
+-- John Piotrowski - HW6 - 7510x
 
-create table junkyard(
+--DDL/Create Table statements
+create table junkyard (
 	jid bigint not null,
 	jname varchar(50) not null,
 	street1 varchar(50) not null,
@@ -13,7 +14,7 @@ create table junkyard(
 	contact_email varchar(60) not null
 );
 
-create table car(
+create table car (
 	cid bigint not null,
 	jid bigint not null,
 	vin varchar(30) not null,
@@ -23,55 +24,48 @@ create table car(
 	color varchar(30) not null,
 	make_model_year_start int not null,
 	make_model_year_end int not null,
-	inventoried_time timestamp
+	inventoried_time timestamp not null
 );
 
-create table part(
+create table part (
 	pid bigint not null,
 	cid bigint not null,
 	part_no varchar(50) not null,
-	description varchar(100) not null,
+	description varchar(100),
+	inventoried_time timestamp not null,
 	color varchar(30),
-	sale_price_cents int,
-	sale_time timestamp not null
+	sale_time timestamp
 );
 
-create table price_history(
+create table price_history (
 	phid bigint not null,
 	pid bigint not null,
 	price_cents int not null,
 	start_time timestamp not null
 );
 
--- query 'left side mirror from a white 2013 Ford Mustang'
-with exact_car_matches as (
-select cid
-from car
-where make = 'Ford'
-	and model = 'Mustang' 
-	and year = 2013
-),
-compatibile_car_matches as (
+-- Query to find 'left side mirror from a white 2013 Ford Mustang'
+with compatible_car_matches as (
 	select cid
-	rom car
+	from car
 	where make = 'Ford'
 	and model = 'Mustang' 
 	and make_model_year_start <= 2013
 	and make_model_year_end >= 2013
 ),
+all_compatible_parts as (
 select *
 from part a
-left join exact_car_matches b on a.cid = b.cid
-left join compatibile_car_matches c on a.cid = c.cid
-where a.part_no = 'left side mirror'
-
-
--- add junkyard event
-
--- add car evnt
-
--- add part event
-
--- add price event
-
--- sell part event
+inner join compatible_car_matches b on a.cid = b.cid
+where a.part_no = 'left side mirror (part_no)'
+),
+parts_by_price_start_time as (
+	select *,
+	row_number() over (partition by a.pid order by b.start_time desc) as row_num
+	from all_compatible_parts a
+	inner join price_history b on a.pid = b.pid
+)
+select *
+from parts_by_price_start_time
+where row_num = 1
+;
