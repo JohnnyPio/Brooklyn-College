7d0d67af5d2d4e7325318e6595b6b1a525544e0f
JohnnyPio <john.s.piotrowski@gmail.com>
2025-04-29 17:10:07 -0400

    HW8 finalized

--- CISC_7510X/HW8/JohnPiotrowski_CISC 7510X HW#8.sql
+++ CISC_7510X/HW8/JohnPiotrowski_CISC 7510X HW#8.sql
@@ -1,5 +1,8 @@
 -- John Piotrowski - 7510X - HW8
 
+-- The run time of the below query in big-O notation is O(N*logN). By far the most time-consuming operation is the potential_violators self-join. However, now that we are joining on 
+-- a key (i.e. the 3 region calculations) - it is no longer a nested loop join and is now a sort-merge, making the time of calculation shortened from O(N^2) to O(N*logN).
+
 with airborne_cars as (
 	select *
 	from car
@@ -15,8 +18,12 @@ regions_of_cars as (
 	from airborne_cars 
 ),
 potential_violators as (
-	select *,
-	sqrt((a.lat_ft-b.lat_ft)^2 + (a.long_ft - b.long_ft)^2 + (a.altitude - b.altitude)^2) as distance_between
+	select 
+		*,
+		a.cid as a_cid,
+		b.cid as b_cid,
+		a.gps_timestamp as a_timestamp,
+		sqrt((a.lat_ft-b.lat_ft)^2 + (a.long_ft - b.long_ft)^2 + (a.altitude - b.altitude)^2) as distance_between
 	from regions_of_cars a
 	inner join regions_of_cars b on a.gps_timestamp = b.gps_timestamp 
 			and a.cid < b.cid
@@ -28,6 +35,16 @@ actual_violators as (
 	select *
 	from potential_violators 
 	where distance_between <= 55
+),
+deduped_violations as (
+    select 
+        a_cid, 
+        b_cid, 
+        a_timestamp,
+        distance_between,
+        row_number() over (partition by a_cid, b_cid order by a_timestamp) as rn
+    from actual_violators
 )
 select *
-from actual_violators 
+from deduped_violations
+where rn = 1
