61148d4cf50fb9432cc562e38f5df72c5aad1d19
JohnnyPio <john.s.piotrowski@gmail.com>
2025-05-21 16:04:22 -0400

    Moved some files around

--- CISC_7410X/Final Project/gradient_descent.py
+++ CISC_7410X/Final Project/gradient_descent.py
@@ -1,21 +1,21 @@
 # John Piotrowski - 7410X - Final Project
-# implement a generic gradient descent function in any other programming language that you can pass in a line, curve, and plane. That can take dataset and any equation and find best fit theta. And it should take hyper parametsr ir ha e some felxible method for sealing with hyperparams
+# From WhatsApp: implement a generic gradient descent function in any other programming language that you can pass in a line, curve, and plane. That can take dataset and any equation and find the best fit theta. And it should take hyper-parameters and have some flexible method for sealing with hyperparams
 
 # Increase python's recursion limit to be greater than the default 1000 :)
 import sys
 sys.setrecursionlimit(2000)
 
-# Line
+# Line params
 xs = [2.0, 1.0, 4.0, 3.0]
 ys = [1.8, 1.2, 4.2, 3.3]
 theta_line = [0.0, 0.0]
 
-# Final Quad
+# Quad params
 quad_xs = [-1.0, 0.0, 1.0, 2.0, 3.0]
 quad_ys = [2.55, 2.1, 4.35, 10.2, 18.25]
 theta_quad = [0.0, 0.0, 0.0]
 
-# Plane
+# Plane params
 plane_xs = [[1.0, 2.05],[1.0, 3.0],[2.0, 2.0],[2.0, 3.91],[3.0, 6.13],[4.0, 8.09]]
 plane_ys = [13.99, 15.99, 18.0, 22.4, 30.2, 37.94]
 theta_plane = [[0.0, 0.0], 0.0]
@@ -37,7 +37,6 @@ def our_dot(w, t):
 plane = lambda ts: \
     lambda theta: \
         [(our_dot(theta[0],t)) + theta[1] for t in ts]
-
 
 def pred_ys(target, xs, theta):
     return target(xs)(theta)
@@ -68,7 +67,7 @@ def contains_tensor(theta):
     else:
         return False
 
-# Using central difference approximation: https://en.wikipedia.org/wiki/Finite_difference
+# I couldn't ever find what was under the hood of Malt's "gradient-of" function, so I decided to use the central difference approximation: https://en.wikipedia.org/wiki/Finite_difference
 def gradient_of_list(f, theta, change_of_theta):
     gradient = []
     for l1 in range(len(theta)):
@@ -86,7 +85,7 @@ def gradient_of_list(f, theta, change_of_theta):
         gradient.append(total_change_in_f/total_change_in_theta)
     return gradient
 
-# Needed to add flatten method (pg. 184) for the tensor
+# Add flatten method (pg. 184) for the tensor case
 def flatten(theta):
     flattened_list = []
     original_nested_list_structure = []     # Only will work for tensors of rank 2, good enough for now.
@@ -99,7 +98,7 @@ def flatten(theta):
             flattened_list.append(param)
     return flattened_list, original_nested_list_structure
 
-# In general, I was surprised by how few resources are out there for unflattening a list with a structure - could be a good opp. for a library
+# In general, I was surprised by how few resources are out there for unflattening a list via its original structure - could be a good opp. for a library
 def unflatten(flattened_list, structure):
     original_list = []
     index = 0
@@ -131,7 +130,6 @@ def revise(f, revs, theta):
     else:
         return revise(f, revs - 1, f(theta))
 
-
 def gradient_descent(obj, theta, alpha, revs):
     def f(big_theta):
         gradient = gradient_of(obj, big_theta)
@@ -141,14 +139,14 @@ def gradient_descent(obj, theta, alpha, revs):
                 weights.append(big_theta[0][i] - alpha * gradient[0][i])
             return [weights, big_theta[1] - alpha * gradient[1]]
         else:
-            return [param - alpha * gradient for param, gradient in zip(big_theta, gradient)]
+            return [big_theta[i] - alpha * gradient[i] for i in range(len(big_theta))]
     return revise(f, revs, theta)
 
 line_grad_descent = gradient_descent(l2_loss(line)(xs,ys), theta_line, 0.01, 1000)
-print(f"gradient descent is {line_grad_descent}")
+print(f"Gradient descent of line is {line_grad_descent}")
 
 quad_grad_descent = gradient_descent(l2_loss(quad)(quad_xs,quad_ys), theta_quad, 0.001, 1000)
-print(f"gradient descent is {quad_grad_descent}")
+print(f"Gradient descent of quad is {quad_grad_descent}")
 
 plane_grad_descent = gradient_descent(l2_loss(plane)(plane_xs,plane_ys), theta_plane, 0.001, 1000)
-print(f"gradient descent is {plane_grad_descent}")
+print(f"Gradient descent of plane is {plane_grad_descent}")
